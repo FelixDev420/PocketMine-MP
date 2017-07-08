c131e30cf789c8d48e3f7c7de861ebf746880195
GamingTrend <rafeole123@gmail.com>
2017-07-08 17:15:34 +0200

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -18,3 +18,19 @@ if exist PocketMine-MP.app
                 set POCKETMINE_FILE=src/pocketmine/PocketMine.php
         ) else ( 
                 echo "Couldn't find a valid PocketMine-MP instalation
+                
+                pause
+                
+                exit 1
+        )
+)
+
+REM if exist bin\php\php_wxwidgets.dll (
+REM    %PHP_BINARY% %POCKETMINE_FILE% --enable gui %*
+REM ) else (
+        if exist bin\mintty.php (
+                start"" bin\mintty.php -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="DejaVu Sans Mono" -o FontHeight=10 -o CursorType=0 -o CurserBlinks=1 -h error -t "PocketMine MP" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*#
+        ) else (
+                %PHP_BINARY% -c bin\php%POCKETMINE_FILE% %*
+        )
+   REM )
