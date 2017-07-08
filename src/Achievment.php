bf2259c202a6ba2e3370c3dcf469ad7ee42b728a
GamingTrend <rafeole123@gmail.com>
2017-07-08 21:08:20 +0200

    Update Achievment.php

--- src/Achievment.php
+++ src/Achievment.php
@@ -1,1 +1,140 @@
 
+This repository
+Pull requests
+Issues
+Marketplace
+Gist
+ @GamingTrend
+ Watch 215
+  Star 1,204
+ Fork 698 PocketMine/PocketMine-MP
+ Code  Issues 144  Pull requests 32  Projects 0 Insights 
+Branch: master Find file Copy pathPocketMine-MP/src/pocketmine/Achievement.php
+cde2d39  on 21 May 2015
+@shoghicp shoghicp New statistics system
+1 contributor
+RawBlameHistory    
+138 lines (123 sloc)  2.88 KB
+<?php
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+namespace pocketmine;
+use pocketmine\event\TranslationContainer;
+use pocketmine\utils\TextFormat;
+/**
+ * Handles the achievement list and a bit more
+ */
+abstract class Achievement{
+	/**
+	 * @var array[]
+	 */
+	public static $list = [
+		/*"openInventory" => array(
+			"name" => "Taking Inventory",
+			"requires" => [],
+		),*/
+		"mineWood" => [
+			"name" => "Getting Wood",
+			"requires" => [ //"openInventory",
+			],
+		],
+		"buildWorkBench" => [
+			"name" => "Benchmarking",
+			"requires" => [
+				"mineWood",
+			],
+		],
+		"buildPickaxe" => [
+			"name" => "Time to Mine!",
+			"requires" => [
+				"buildWorkBench",
+			],
+		],
+		"buildFurnace" => [
+			"name" => "Hot Topic",
+			"requires" => [
+				"buildPickaxe",
+			],
+		],
+		"acquireIron" => [
+			"name" => "Acquire hardware",
+			"requires" => [
+				"buildFurnace",
+			],
+		],
+		"buildHoe" => [
+			"name" => "Time to Farm!",
+			"requires" => [
+				"buildWorkBench",
+			],
+		],
+		"makeBread" => [
+			"name" => "Bake Bread",
+			"requires" => [
+				"buildHoe",
+			],
+		],
+		"bakeCake" => [
+			"name" => "The Lie",
+			"requires" => [
+				"buildHoe",
+			],
+		],
+		"buildBetterPickaxe" => [
+			"name" => "Getting an Upgrade",
+			"requires" => [
+				"buildPickaxe",
+			],
+		],
+		"buildSword" => [
+			"name" => "Time to Strike!",
+			"requires" => [
+				"buildWorkBench",
+			],
+		],
+		"diamonds" => [
+			"name" => "DIAMONDS!",
+			"requires" => [
+				"acquireIron",
+			],
+		],
+	];
+	public static function broadcast(Player $player, $achievementId){
+		if(isset(Achievement::$list[$achievementId])){
+			$translation = new TranslationContainer("chat.type.achievement", [$player->getDisplayName(), TextFormat::GREEN . Achievement::$list[$achievementId]["name"]]);
+			if(Server::getInstance()->getConfigString("announce-player-achievements", true) === true){
+				Server::getInstance()->broadcastMessage($translation);
+			}else{
+				$player->sendMessage($translation);
+			}
+			return true;
+		}
+		return false;
+	}
+	public static function add($achievementId, $achievementName, array $requires = []){
+		if(!isset(Achievement::$list[$achievementId])){
+			Achievement::$list[$achievementId] = [
+				"name" => $achievementName,
+				"requires" => $requires,
+			];
+			return true;
+		}
+		return false;
+	}
+}
